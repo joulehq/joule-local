1e0fc220fa07f4496b351e914765c9b4654cc900
Jaisen Mathai <jaisen@jmathai.com>
2016-01-28 01:07:09 -0800

    Add initial files.

---
+++ index.js
@@ -0,0 +1,48 @@
+#!/usr/bin/env node
+var fs = require('fs');
+var path = require('path');
+var yaml = require('js-yaml');
+var wait = require('wait.for');
+
+var handlerFile = path.resolve(process.cwd() + '/index.js');
+var jouleFile = path.resolve(process.cwd() + '/../.joule.yml');
+var eventsFile = path.resolve(process.cwd() + '/../events.json');
+
+var context = {
+  succeed: function(input) {
+    console.log(input);
+  },
+  fail: function(input) {
+    console.log(input);
+  }
+};
+
+var events;
+
+try {
+  events = JSON.parse(fs.readFileSync(eventsFile, 'utf8'));
+  if(!events) {
+    console.log('Unable to JSON parse ' + eventsFile + '.');
+    return;
+  }
+} catch(e) {
+  if (e.code == 'ENOENT') {
+    console.log('Failed to read ' + eventsFile + '.');
+  } else {
+    console.log('Exception when trying to access or parse ' + eventsFile + '.');
+  }
+  return;
+}
+
+var init = function() {
+};
+
+var run = function(payload) {
+  var handler = require(handlerFile);
+  wait.for(handler.handler, payload, context);
+};
+
+init();
+for(var i=0; i<events.length; i++) {
+  wait.launchFiber(run, events[i]);
+}
